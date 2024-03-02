48daf90d260b3cf8178f9a0153a8a2ff9c1e0a8b
Ridam-Kukde <ridamkukde01@gmail.com>
2024-03-03 02:32:57 +0530

    worked

--- Mern/backend/src/app.js
+++ Mern/backend/src/app.js
@@ -1,5 +1,4 @@
 const express= require ('express');
-const {connectedToMongoDB}= require('./configuration/dbConfig');
 const app= express();
 const UserRoutes= require("./routes/userRoute");
 const bodyParser = require("body-parser");
@@ -8,11 +7,11 @@ const PORT= process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
-connectedToMongoDB().then(()=>{
+
     app.listen(PORT, ()=>{
         console.log(`Server is running on http://localhost:${PORT}`);
     })
-});
+
  
 app.use("api/user", UserRoutes);
 
